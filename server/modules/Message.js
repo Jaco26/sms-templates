3ef45d52948e75a26fbba363addd9934a356734f
Jacob Albright <jacob.albright23@gmail.com>
2018-05-03 16:53:02 -0500

    Added some comments to the buildGreeting method in the Message class

--- server/modules/Message.js
+++ server/modules/Message.js
@@ -7,6 +7,8 @@ class Message {
         this.guest = guest;
         this.company = company;
         this.template = template;
+        // The keys in keyMap become the placeholders that the user can enter 
+        // when they create new template content on the UI.
         this.keyMap = {
             'NAME': this.guest.firstName,
             'FULL_NAME': this.guest.firstName + ' ' + this.guest.lastName,
@@ -18,27 +20,46 @@ class Message {
     }
 
     generate () {
-        let message = `${this.buildGreeting()} ${this.buildWelcome()} ${this.buildRoomInfo()} ${this.buildAction()}`;
-        return message;
+        return `${this.buildGreeting()} ${this.buildWelcome()} ${this.buildRoomInfo()} ${this.buildAction()}`;
     }
 
     buildGreeting () {
+        // Get the time of day ("morning", "afternoon", "evening") adjusted 
+        // for the timezone of the selected company
         let timeOfDay = this.timeOfDayForGreeting();  
-        return this.template.greeting[timeOfDay].split(' ').map(word => {
+        // Get the string associated with a given time of day key in this.template.greeting.
+        // Then, split that string into an array where each element is a word from the string
+        // and for each word, do some stuff. After the stuff is done, add the result to a new 
+        // array called "result"... 
+        let result = this.template.greeting[timeOfDay].split(' ').map(word => {
+            // For each word...
+            // declare a variable "punctuation"
             let punctuation;
+            // if the word includes a non-word character ...
             if (/\W/g.test(word)) {
+                // set punctuation equal to an array of all the non-word characters in the word
                 punctuation = word.match(/\W/g);
             }
+            // if the word, absent of any non-word characters, is a key in this.keyMap, do this...
             if (this.keyMap[word.replace(/\W/g, '')]) {
+                // if punctuation has a value
                 if (punctuation) {
+                    // return the value associated with the word, absent punctuation, as a key in keyMap 
+                    // and then append the punctuation to that value
                     return word = this.keyMap[word.replace(/\W/g, '')] + punctuation.join('');
+                    // otherwise...
                 } else {
+                    // do the same thing but without trying to append punctuation
                     return word = this.keyMap[word.replace(/\W/g, '')]
                 }
             } else {
+                // if the word is not a key in keyMap, return it
                 return word;
             }
-        }).join(' ');
+        });
+        // after the array stored in "result" is finished being built, join the 
+        // contents with a space between each one and return the resulting string.
+        return result.join(' ');
     }
 
     buildWelcome () {
@@ -105,7 +126,9 @@ class Message {
         let { year, month, day, hour, minute } = new ReservationDate(start).convertToUTC();
         // Get reservation date and time adjusted for company's timezone 
         let reservationStartDate = DateTime.utc(year, month, day, hour, minute).setLocale('en-US').setZone(companyTimezone); 
+        // create a 'maxMorning' variable with a value that represents the maximum time in a day that counts as morning
         let maxMorning = reservationStartDate.startOf('day').plus({ hours: 12 }).ts;
+        // do the same for 'maxAfternoon' and 'maxEvening'
         let maxAfternoon = reservationStartDate.startOf('day').plus({ hours: 17 }).ts;
         let maxEvening = reservationStartDate.startOf('day').plus({ hours: 24 }).ts;
         if (reservationStartDate.ts < maxMorning ) {
